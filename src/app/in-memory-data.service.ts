a0ddd1322bd0f56b6a15631e56e6536f5224f1ba
Van J. Wilson <van.wilson1992@alumni.unc.edu>
2018-09-25 20:20:50 -0400

    Complete pt. 8, HTTP client service and observables

---
+++ src/app/in-memory-data.service.ts
@@ -0,0 +1,19 @@
+import { InMemoryDbService } from 'angular-in-memory-web-api';
+
+export class InMemoryDataService implements InMemoryDbService {
+  createDb() {
+    const heroes = [
+      { id: 11, name: 'Mr. Nice', color: 'cyan', superPower: 'Charm an adversary' },
+      { id: 12, name: 'Narco', color: 'rainbow', superPower: 'Put an opponent in a trance' },
+      { id: 13, name: 'Bombasto', color: 'orange', superPower: 'Create a shock wave' },
+      { id: 14, name: 'Celeritas', color: 'white', superPower: 'Run like the wind' },
+      { id: 15, name: 'Magneta', color: 'pink', superPower: 'Bedazzle a group of people' },
+      { id: 16, name: 'RubberMan', color: 'red', superPower: 'Contort himself into small spaces' },
+      { id: 17, name: 'Dynama', color: 'green', superPower: 'Generate an electric field' },
+      { id: 18, name: 'Dr IQ', color: 'purple', superPower: 'Untangle complicated knots' },
+      { id: 19, name: 'Magma', color: 'yellow', superPower: 'Super-heat materials' },
+      { id: 20, name: 'Windstorm', color: 'blue', superPower: 'Summon a tornado' }
+        ];
+    return {heroes};
+  }
+}
